99db58373df743f87093ded4e468c9935ce16a3a
Gusko Maksim <gusko.maksim.n@gmail.com>
2024-12-23 16:55:03 +0300

    new setup

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ if [ "$(git config --global --get user.name)" != "Gusko Maksim" ]; then
     git config --global user.name "Gusko Maksim"
 else
     echo "Имя пользователя Git уже настроено."
-fi &&
+fi
 
 # Проверка и настройка Git user.email
 
@@ -15,7 +15,7 @@ if [ "$(git config --global --get user.email)" != "gusko.maksim.n@gmail.com" ]; 
     git config --global user.email "gusko.maksim.n@gmail.com"
 else
     echo "Email пользователя Git уже настроен."
-fi &&
+fi
 
 # Проверка существования и создание SSH-ключей
 if [ ! -f ~/.ssh/id_rsa ]; then
@@ -23,19 +23,51 @@ if [ ! -f ~/.ssh/id_rsa ]; then
     ssh-keygen -o -t rsa -b 4096 -C "gusko.maksim.n@gmail.com" -N "" -f ~/.ssh/id_rsa
 else
     echo "SSH-ключи уже существуют."
-fi &&
+fi
 
-# Проверка наличия и создание GPG-ключей
-if ! gpg --list-keys | grep -q "gusko.maksim.n@gmail.com"; then
+
+# Проверка существования ключа
+if ! gpg --list-keys "gusko.maksim.n@gmail.com" &> /dev/null; then
     echo "GPG-ключ не найден, создаем новый..."
-    gpg --full-generate-key
+
+    # Создание конфигурационного файла
+    cat <<EOF > gpg-key.conf
+    %no-protection
+    Key-Type: 1
+    Key-Length: 4096
+    Subkey-Type: 1
+    Subkey-Length: 4096
+    Name-Real: Maksim Gusko
+    Name-Email: gusko.maksim.n@gmail.com
+    Expire-Date: 0
+EOF
+
+    # Генерация ключа
+    gpg --batch --generate-key gpg-key.conf
+
+    # Удаление временного файла
+    rm -f gpg-key.conf
+
+    echo "GPG-ключ успешно создан."
 else
     echo "GPG-ключ уже существует."
-fi &&
+fi
 
-# Обновление зеркал и установка пакетов
-sudo pacman-mirrors -g &&
-sudo pacman -Syyuu --noconfirm \
+
+# Проверка наличия и создание GPG-ключей
+#if ! gpg --list-keys | grep -q "gusko.maksim.n@gmail.com"; then
+#    echo "GPG-ключ не найден, создаем новый..."
+#    gpg --full-generate-key
+#else
+#    echo "GPG-ключ уже существует."
+#fi
+
+sudo pacman-mirrors -g
+sudo pacman-key --refresh-keys
+sudo pacman -Sy --noconfirm archlinux-keyring
+sudo pacman -Syyuu --noconfirm
+
+sudo pacman -S --noconfirm \
     yay \
     flatpak \
     yakuake \
@@ -57,15 +89,15 @@ sudo pacman -Syyuu --noconfirm \
     kde-applications-meta \
     manjaro-kde-settings \
     sddm-breath-theme \
-    manjaro-settings-manager &&
+    manjaro-settings-manager
 
-pamac install base-devel --no-confirm &&
+pamac install base-devel --no-confirm
 
 # Включение службы sddm
-sudo systemctl enable sddm.service --force &&
+sudo systemctl enable sddm.service --force
 
 # Добавление Flathub репозитория
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &&
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Скачивание программ
 sudo pacman -S --noconfirm \
@@ -73,10 +105,11 @@ sudo pacman -S --noconfirm \
     keepassxc \
     doublecmd \
     telegram-desktop \
-    steam &&
+    steam
+
 flatpak install -y flathub \
-    com.opera.Opera \
     com.google.Chrome \
+    com.opera.Opera \
     com.visualstudio.code \
     com.jetbrains.PyCharm-Community \
     md.obsidian.Obsidian \
@@ -86,11 +119,10 @@ flatpak install -y flathub \
     com.sublimetext.three \
     com.unity.UnityHub \
     org.qbittorrent.qBittorrent\
-    &&
 
-chmod +x setup_zsh.sh &&
-chmod +x setup_vim.sh &&
-chmod +x add_yakuake_autostart.sh &&
+chmod +x setup_zsh.sh
+chmod +x setup_vim.sh
+chmod +x add_yakuake_autostart.sh
 
 
 
